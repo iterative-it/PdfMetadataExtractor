2a35dd99ec003c1feb39c61fa4b0c01230ba101c
David Winchurch <david.winchurch@iterativeit.com>
2022-04-25 13:04:33 +0100

    Testing itext7

---
+++ PdfMetadataExtractor/Program.cs
@@ -0,0 +1,16 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.XMP;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+PdfDocument pdfDoc = new PdfDocument(new PdfReader(@""));
+
+XMPUtils.
+
+pdfDoc.GetXmpMetadata
+
+PdfPage page = pdfDoc.GetFirstPage();
+//page.SetXmpMetadata(XMPMetaFactory.Create());
+
+pdfDoc.Close();
